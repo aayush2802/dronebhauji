83f33ee53bfbd747f4041029f4febe9e89da1664
Aayush Verma <aayush.verma2802@gmail.com>
2024-12-03 13:33:38 +0530

    req

--- dronebabu/backend/app.py
+++ dronebabu/backend/app.py
@@ -7,7 +7,7 @@ import os
 app = FastAPI()
 
 # Load the pre-trained KNN model
-model_path = os.path.join(os.getcwd(), "D:\dronebabu - Copy\dronebabu\backend\knn_model.pkl")
+model_path = "knn_model.pkl"
 with open(model_path, "rb") as f:
     model = pickle.load(f)
 
